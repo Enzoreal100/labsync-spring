3a8300fe121c94ec7894ab13fc72007cd075d53d
enzoreal100 <enzoreal100@gmail.com>
2025-10-02 22:36:44 -0300

    fix: update Swagger configuration to include production server URL

--- src/main/java/com/config/SwaggerConfig.java
+++ src/main/java/com/config/SwaggerConfig.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,6 +20,7 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Labsync API")
                         .version("1.0")
-                        .description("API documentation for Labsync application"));
+                        .description("API documentation for Labsync application"))
+                    .servers(List.of(productionServer));
     }
 }
