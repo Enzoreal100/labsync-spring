ba3fc29b3c59ae10d606ff9b6ef1c4ca664bf75c
enzoreal100 <enzoreal100@gmail.com>
2025-10-02 22:31:28 -0300

    fix: enhance Swagger configuration with production server URL

--- src/main/java/com/config/SwaggerConfig.java
+++ src/main/java/com/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +12,8 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        Server productionServer = new Server();
+        productionServer.setUrl("https://homol-labsync.ddns.net");
         return new OpenAPI()
                 .info(new Info()
                         .title("Labsync API")
