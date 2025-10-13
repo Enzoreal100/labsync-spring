639ebc2fe359a785849101734f31bbf0e3264a01
enzoreal100 <enzoreal100@gmail.com>
2025-10-12 21:54:39 -0300

    feat: added validation logic

--- src/main/java/com/controller/AuthController.java
+++ src/main/java/com/controller/AuthController.java
@@ -1,8 +1,48 @@
 package com.controller;
 
-import org.springframework.stereotype.Controller;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dto.auth.LoginDTO;
+import com.service.AuthService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
+@Tag(name = "Auth", description = "Auth API")
 public class AuthController {
     
+    @Autowired
+    private AuthService authService;
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
+
+    @PostMapping
+    @Operation(summary = "Login", description = "Login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginDTO loginDTO) {
+        return ResponseEntity.ok("Id:" + loginDTO.getId() + "pw:" + loginDTO.getPassword());
+    }
 }
