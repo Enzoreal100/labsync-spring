baedd6a7c8a677eae8d270d3baba055ddde97cb7
enzoreal100 <enzoreal100@gmail.com>
2025-10-21 10:38:14 -0300

    feat: added verification to superuser routes

--- src/main/java/com/config/WebConfig.java
+++ src/main/java/com/config/WebConfig.java
@@ -1,11 +1,16 @@
 package com.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private PositionInterceptor positionInterceptor;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -14,5 +19,12 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(positionInterceptor)
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/auth/login", "/api/auth/refresh", "/api/stock/take");
     }
 }
